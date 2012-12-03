6ff1b01c9faef4f7a481c52ef3fe7fa977c57bee
Adam Kaplan <akaplan@gilt.com>
2012-12-03 16:31:20 -0500

    Added zshrc, setup.sh bootstrap, README and vim submodule

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+LN="ln -s"
+SOURCE=`pwd`
+
+`git submodule init 1>/dev/null`
+`git submodule update 1>/dev/null`
+
+# vim (core)
+$LN $SOURCE/dot_vim/vimrc ~/.vimrc
+
+# vim (pathogen)
+mkdir ~/.vim
+$LN $SOURCE/dot_vim/autoload ~/.vim/autoload
+$LN $SOURCE/dot_vim/bundle ~/.vim/bundle
+$LN $SOURCE/dot_vim/ftdetect ~/.vim/ftdetect
+$LN $SOURCE/dot_vim/indent ~/.vim/indent
+$LN $SOURCE/dot_vim/plugin ~/.vim/plugin
+$LN $SOURCE/dot_vim/syntax ~/.vim/syntax
+
+# tmux
+$LN $SOURCE/tmux.conf ~/.tmux.conf
+
+# oh-my-zsh
+$LN $SOURCE/zshrc ~/.zshrc
